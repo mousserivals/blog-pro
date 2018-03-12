8744d94f7f8ea863efd6f6670e8b6f0cb187f6d9
mousserivals <fitoussitephane@gmail.com>
2018-03-12 08:51:54 +0200

    suite issue 2 - router

--- web/index.php
+++ web/index.php
@@ -2,21 +2,21 @@
 
 require __DIR__."/../vendor/autoload.php";  
   
-use Lib\Router;
+use Lib\Router\Router;
 
 $url = [];
-$url1 = "sorti-entre-amis" ;  
-$url2 = "draculas/au-cinema" ;  
-$url3 = "sorties-cinema-de-la-semaine/jour-07-3-2018" ;  
-$url4 = "blog/add" ;  
-$url5 = "blog/update/5" ;  
-$url6 = "blog/1-20" ;  
+$url1 = "/sorti-entre-amis" ;
+$url2 = "/draculas/au-cinema" ;
+$url3 = "/sorties-cinema-de-la-semaine/jour-07-3-2018" ;
+$url4 = "/admin/blog/add" ;
+$url5 = "/admin/blog/update/5";
+$url6 = "/admin/blog/1-20" ;
 
-$urlFausse1 = "categories";
-$urlFausse2 = "promo/view";
-$urlFausse3 = "blog/view/7";
+$urlFausse1 = "/categories";
+$urlFausse2 = "/promo/view";
+$urlFausse3 = "/blog/view/7";
 
-$router = new Router($url3);
-$url = $router->getUrl();
+$router = new Router();
+$url = $router->navigate($url1);
 
 echo $url;
