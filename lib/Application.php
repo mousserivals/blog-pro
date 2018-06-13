abc1618186f42f165fa5b86f080d77aeb2e8d2bb
mousserivals <fitoussitephane@gmail.com>
2018-06-13 10:49:58 +0300

    10 - Blog design with pagination

--- lib/Application.php
+++ lib/Application.php
@@ -13,26 +13,25 @@ class Application {
     public function __construct() {
         $this->request = new Request();
         $this->router = new Router();
-
     }
-    
+
     public function appRun() {
 
         $this->router->get('/', 'Post#home', 'Post.home');
-        $this->router->get('/posts', 'Post#index', 'Post.index');
+        $this->router->get('/blog', 'Post#index', 'post.index');
+        $this->router->get('/blog/:pagination', 'Post#index', 'post.index')->with('pagination', '[0-9]+');
+//        $this->router->get('/posts', 'Post#index', 'Post.index');
         $this->router->get('/posts/:id', 'Post#show', 'Post.show');
         $this->router->get('/posts/:id-:slug', function($id, $slug) {
                     echo "Afficher $slug : $id";
                 }, 'post.show')->with('id', '[0-9]+')
                 ->with('slug', '[a-z\-0-9]+');
-        
+
 
         $this->router->post('/posts/:id', function($id) {
             echo 'Poster l\'articles ' . $id;
         });
         $this->router->getRoute($this->request, $this->router);
-
-        
     }
 
 }
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -11,37 +11,38 @@ use Src\Manager\PostManager;
 class PostController extends Controller {
 
     function home() {
+        
 
-      $this->render('Post/home.html.twig',['title' => 'Bienvenu visiteur']);
+        $this->render('Post/home.html.twig', ['title' => 'Bienvenu visiteur']);
     }
 
-    function index() {
-        $manager = $this->database()->getManagerOf(Comment::class);
-        $article = $manager->commentByPost(1);
-        
-        $this->render('Post/index.html.twig',['title' => 'Bienvenu visiteur']);
-
+    function index($newpage = null) {
+//        $manager = $this->database()->getManagerOf(Comment::class);
+//        $article = $manager->commentByPost(1);
+        $manager = $this->database()->getManagerOf(Post::class);
+        $paginations = $manager->paginate($newpage);
+        $this->render('Post/index.html.twig', ['pagination' => $paginations]);
     }
 
     function show($id) {
 
 //        $post = new Post();
-//        $post->setId(5);
-//        $post->setTitle("titlemodified");
+////        $post->setId();
+//        $post->setUserId(1);
+//        $post->setTitle("title10");
 //        $post->setContent("titlemodified");
 //        $post->setCategoryId(1);
-//        $post->setDate(date("Y-m-d H:i:s"));
+//        $post->setDateCreated(date("Y-m-d H:i:s"));
 //         $postManager = $this->database()->getManagerOf(Post::class)->add($post);
-
-        $manager = $this->database()->getManagerOf(Post::class);
-        $article = $manager->find(1);
+//        $manager = $this->database()->getManagerOf(Post::class);
+//        $article = $manager->find(1);
 //        $article->setTitle('titre modifie');
 //        $article->setContent('contenu modifie');
 //        $manager->modify($article);
 //        $manager->delete($article);
 //echo $article;
-var_dump($article);
-exit();
+//var_dump($article);
+//exit();
     }
 
 }
